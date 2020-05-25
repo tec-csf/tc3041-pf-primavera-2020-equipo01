32518f1bac7ca03e98df0ec709b2a196dc9be884
Alejandro Guti <axguti@gmail.com>
2020-05-24 20:46:56 -0500

    fixed typo in micro

--- microservices/micro/api/routes.js
+++ microservices/micro/api/routes.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const controller = require('./controller');
+var controller = require('./controller');
 
-module.exports = functions(app) {
+module.exports = function(app) {
     app.route('/about')
-    .get(controller.about);
+        .get(controller.about);
     app.route('/distance/:zipcode1/:zipcode2')
     .get(controller.getDistance);
 };
