af2d194d0338827e7030f0875be0b758c65e43d5
Alejandro Guti <axguti@gmail.com>
2020-05-24 20:30:11 -0500

    added css to qr and started microservice skeleton

---
+++ microservices/micro/api/controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var properties = require('../package.json');
+var distance = require('../service/distance');
+
+var controllers = {
+    about: function(req, res) {
+        var aboutInfo = {
+            name: properties.name,
+            version: properties.version
+        }
+        res.json(aboutInfo);
+    },
+    getDistance: function(req, res){
+        distance.find(req, res, function(err, dist) {
+            if(err){
+                res.send(err);
+            }
+            res.json(dist);
+        });
+    },
+};
+
+module.exports = controllers;
