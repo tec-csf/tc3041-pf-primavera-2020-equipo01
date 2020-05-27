cd851eba24391496083e8a012608f639b50d8629
Daniel <danielcharua@hotmail.com>
2020-05-27 12:45:47 -0500

    docker files change

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hwk1-adb.uc.r.appspot.com/',
-  auth: 'http://localhost:3000/',
+  apiUrl: '10.4.3.6:80',
+  auth: '10.4.12.66:80',
+  CSV: '10.4.10.178:80',
   maps : 'AIzaSyAjfsVYLl5-ub-xmXPRNQUmKt3nMOKZ1Pk',
 };
