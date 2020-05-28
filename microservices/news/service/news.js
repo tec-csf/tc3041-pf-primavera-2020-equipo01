5f8dc72be4a9dc0e3d84baf47e7470e010a44eff
Alejandro Guti <axguti@gmail.com>
2020-05-27 21:21:46 -0500

    modified query to api

--- microservices/news/service/news.js
+++ microservices/news/service/news.js
@@ -3,20 +3,22 @@ var request = require('request');
 const apiKey = process.env.NEWS_API_KEY || "73930ae77d0d4ebaae0b7db2cc74b0f5";
 const query = "Coronavirus";
 const date = "2020-05-28";
+const category = "general";
 const sortBy = "popularity";
-const newsURL = "http://newsapi.org/v2/everything?";
-// http://newsapi.org/v2/everything?q=Coronavirus&from=2020-05-28&sortBy=popularity&apiKey=
+const newsURL = "http://newsapi.org/v2/top-headlines?";
+// http://newsapi.org/v2/top-headlines?q=Coronavirus&from=2020-05-28&category=general&sortBy=popularity&apiKey=73930ae77d0d4ebaae0b7db2cc74b0f5
 var news = {
     find: function(req, res, next){
         request(newsURL 
             + "q=" + query
             + "&from=" + date
-            + "sortBy=" + sortBy
+            + "&category=" + category
+            + "&sortBy=" + sortBy
             + "&apiKey=" + apiKey,
             function(error, response, body){
-                if(!error && response.statusCode == 200){
+                if(!error && response.statusCode == 200){ // if no error   
                     response = JSON.parse(body);
-                    res.send(response);
+                    res.send(response.articles);
                 }else{
                     console.log(response.statusCode + response.body);
                     res.send({news: -1});
