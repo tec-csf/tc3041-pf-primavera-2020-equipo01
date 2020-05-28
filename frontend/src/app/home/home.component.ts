6a9f2aa92b9363a13f55a004bcb7e8760a491906
Alejandro Guti <axguti@gmail.com>
2020-05-27 22:15:12 -0500

    Loads news in homepage, change to microservice

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +12,25 @@ export class HomeComponent implements OnInit {
   cases:any;
   deaths:any;
   recovered:any;
+  news: [];
   constructor(
     private http: HttpClient
   ) { }
 
   ngOnInit() {
+    // Stats
     this.http.get('https://api.covid19api.com/world/total').subscribe((res:any) => {
       console.log(res)
       this.cases = res.TotalConfirmed
       this.deaths = res.TotalDeaths
       this.recovered = res.TotalRecovered
+    }, error =>  console.log(error))
+    // News TODO
+    // http://newsapi.org/v2/top-headlines?q=Coronavirus&from=2020-05-28&category=general&sortBy=popularity&apiKey=73930ae77d0d4ebaae0b7db2cc74b0f5
+    // environment.news + 'news'
+    this.http.get('http://newsapi.org/v2/top-headlines?q=Coronavirus&from=2020-05-28&category=general&sortBy=popularity&apiKey=73930ae77d0d4ebaae0b7db2cc74b0f5').subscribe((res:any) => {
+      console.log(res)
+      this.news = res.articles;
     }, error =>  console.log(error))
   }
     
